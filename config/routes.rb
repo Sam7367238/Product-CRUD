8e639939edc29b8109e8da4f6a25bdc87462ef4d
ayman <aymantheremkus11@gmail.com>
2025-09-23 17:36:05 +0300

    Add Routes To Support Cart System

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :products
+  patch "/products/:id/add-to-cart", to: "products#add_to_cart", as: "add_to_cart"
+  patch "/products/:id/remove-from-cart", to: "products#remove_from_cart", as: "remove_from_cart"
+  get "/cart", to: "cart#index", as: "cart"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +14,5 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  root "products#index"
 end
